c7ba3469d5b071bb70643f4dc7589550450e6295
AlokRandive18 <alokbavdekar8278@gmail.com>
2025-12-02 10:48:09 +0530

    Added Area of Circle program

---
+++ CALCULATOR/AreaOfCircle/Area.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace  AreaOfCircle{
+class Area
+{
+    static void Main(string[] args)
+    {
+        double radius = 3.14;
+        double pi = 3.14;
+
+        double area = pi * radius * radius;
+
+        Console.WriteLine("Radius: " + radius);
+        Console.WriteLine("Area of Circle : " + area);
+
+        Console.ReadLine(); // Keeps the console open
+    }
+}
+}
