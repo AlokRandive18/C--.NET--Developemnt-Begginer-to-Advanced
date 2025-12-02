f375c6b4c8dbdf1e2c5405ff33fc2af41dc7aad4
AlokRandive18 <alokbavdekar8278@gmail.com>
2025-12-02 21:43:04 +0530

    Add files via upload

---
+++ DAY 1/Assigment_1/Assigment_1/AreaOfCircle.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+class AreaOfCircle
+{
+    static void Main(String[] args)
+    {
+        double radius = 3.14;
+        double pi = 3.14;
+
+        double area = pi * radius * radius;
+
+        Console.WriteLine("Radius = " + radius);
+        Console.WriteLine("Area of Circle = " + area);
+
+        Console.ReadLine();
+    }
+}
---
+++ DAY 1/Assigment_1/Assigment_1/HeightConversion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Assigment_1
+{
+	public class HeightConversion
+
+
+
+    {
+        static void HeightCoversion()
+        {
+            Console.Write("Enter height in inches: ");
+            double heightInInches = Convert.ToDouble(Console.ReadLine());
+
+            double heightInCM = heightInInches * 2.54;
+
+            Console.WriteLine("Height in CM: " + heightInCM);
+
+            if (heightInCM < 150)
+            {
+                Console.WriteLine("Category: Short");
+            }
+            else if (heightInCM >= 150 && heightInCM <= 180)
+            {
+                Console.WriteLine("Category: Average");
+            }
+            else
+            {
+                Console.WriteLine("Category: Tall");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
